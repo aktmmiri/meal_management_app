44f4f6c28a449bc8510d5610aec449dc789ed79c
aktmmiri <lionsfan0316@gmail.com>
2021-11-24 15:20:52 +0900

    mealモデル作成

---
+++ db/migrate/20211124060947_create_meals.rb
@@ -0,0 +1,12 @@
+class CreateMeals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :meals do |t|
+      t.string :name, null: false
+      t.integer :amount, null: false
+      t.text :info
+      t.integer :time_id, null: false
+      t.integer :calorie, null: false
+      t.timestamps
+    end
+  end
+end
