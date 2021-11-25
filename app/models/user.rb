a51f6faeec48731b38ce667af8f8f750917caa2a
aktmmiri <lionsfan0316@gmail.com>
2021-11-25 12:47:37 +0900

    バリデーションとエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   
   has_many :meals
 
+  with_options presence: true do
+    validates :nickname
+    validates :gender
+    validates :age_before_type_cast, format: { with: /\A[0-9]+\z/, allow_blank: true, message: 'は半角数字で入力してください' }
+    validates :height_before_type_cast, format: { with: /\A[0-9]+\z/, allow_blank: true, message: 'は半角数字で入力してください' }
+    validates :weight_before_type_cast, format: { with: /\A[0-9]+\z/, allow_blank: true, message: 'は半角数字で入力してください' }
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true, message: 'には半角英字と半角数字の両方を含めて設定してください'
 end
