f4d02f4e9da474297e5705f65e4ced5fdb015788
aktmmiri <lionsfan0316@gmail.com>
2021-11-26 11:22:41 +0900

    mealモデルのバリデーション

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,4 +5,10 @@ class Meal < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :meal_time
 
+  with_options presence: true do
+    validates :name
+    validates :amount_before_type_cast, format: { with: /\A[0-9]+\z/, allow_blank: true, message: 'は半角数字で入力してください' }
+    validates :calorie_before_type_cast, format: { with: /\A[0-9]+\z/, allow_blank: true, message: 'は半角数字で入力してください' }
+    validates :time_id, numericality: { other_than: 1, message: 'を選択してください' }
+  end
 end
