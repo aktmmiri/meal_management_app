35ea83152511c280e6840471678387c574c19a87
aktmmiri <lionsfan0316@gmail.com>
2021-12-03 17:41:47 +0900

    マイページでのお気に入り一覧表示のためのインスタンス変数の定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
     if @user.id != current_user.id
       redirect_to root_path 
     end
+    favorites = Favorite.where(user_id: current_user.id).order('created_at DESC').pluck(:meal_id)
+    @favorites = Meal.find(favorites)
   end
 end
