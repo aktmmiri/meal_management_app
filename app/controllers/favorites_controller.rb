10f554377efe1f0989bd8a5567dbde664bfadd4a
aktmmiri <lionsfan0316@gmail.com>
2021-12-03 17:40:52 +0900

    create, destroyアクションの定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,20 @@
 class FavoritesController < ApplicationController
-  def create
+  before_action :set_meal
 
+  def create
+    if @meal.user_id != current_user.id
+      @favorite = Favorite.create(user_id: current_user.id, meal_id: @meal.id)
+    end
   end
 
   def destroy
-    
+    @favorite = Favorite.find_by(user_id: current_user.id, meal_id: @meal.id)
+    @favorite.destroy
+  end
+
+  private
+
+  def set_meal
+    @meal = Meal.find(params[:meal_id])
   end
 end
