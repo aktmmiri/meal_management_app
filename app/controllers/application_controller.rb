8734bd888be5ed9ef33bb58f3da18101f7df7613
aktmmiri <lionsfan0316@gmail.com>
2021-12-03 14:46:34 +0900

    ユーザー情報編集するためのパラメーター取得設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :age, :height, :weight])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :gender, :age, :height, :weight])
   end
 end
